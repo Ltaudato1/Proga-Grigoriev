40195a6ded163b2a6f5e418852a71e3e623cd294
Ltaudato1 <gitta.tawer13@gmail.com>
2024-03-20 10:19:34 +0300

    task3 fixed

--- Sem2Task3/invoker.c
+++ Sem2Task3/invoker.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "structs.h"
+#include "painter.h"
 
 Menu menu;
 
--- Sem2Task4/player.h
+++ Sem2Task4/player.h
@@ -1,3 +1,16 @@
 #pragma once
 
+#ifndef PLAYER_H
+#define PLAYER_H
 
+#include "general.h"
+#include <stdio.h>
+
+typedef struct {
+    Vector2D position;
+    float width;
+    float height;
+    float speed;
+} Paddle;
+
+#endif
